4dcfc2c16f448028e1ecbb17296557ee38bd88a3
cn80393313@gmail.com <AS80393313hj>
2020-01-02 17:53:04 +0800

    1

--- README.md
+++ README.md
@@ -1,42 +1,53 @@
+# 身份证号码、法人和其他组织统一社会信用代码 的合法性校验
 <div align="center">
 
 [![MIT](https://img.shields.io/dub/l/vibe-d.svg?style=flat-square)](http://opensource.org/licenses/MIT)
 
 </div>
 
-# 简介
-这是一个jquery插件，基于$.ajax方法扩展，取消多次重复的请求。
+用于校验 `身份证号码` 和 `法人和其他组织统一社会信用代码` 的工具。(基于 <a href="https://github.com/bluesky335/IDCheck" target="_blank">IDCheck</a>，这是JS的实现方式（搬砖手动狗头）)
 
+计算规则参考国家标准文件：
 
+- **标准号：GB 11643-1999**：[公民身份证号码](http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E)
 
+- **标准号：GB 32100-2015**：[法人和其他组织统一社会信用代码编码规则](http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=24691C25985C1073D3A7C85629378AC0)
+
+> 注：由于早期部分试点地区推行 `法人和其他组织统一社会信用代码` 较早，会存在部分代码不符合国家标准的情况。但它们都是合法的代码，应当另行处理。
+> 例如：
+>
+> **福建恒跃柳工机械销售有限公司**: `91350100M0001TGQXM` 计算出的校验位是 1 和 M 不符
+>
+> **厦门云上晴空航空科技有限公司**:`91350211M0000XUF46` 计算出的校验位是 R 和 6 不符
+>
+> **厦门黑脉网络科技有限公司**`91350203M0001FUE2P` 计算出的校验位是 J 和 P 不符
 # 浏览器支持
-1.  支持所有现代浏览器
-2.  IE5+
+IE5+和现代浏览器
+# 使用方法
 
-# 使用
-1. `git clone https://github.com/ChrisDowney1996/jquery-fetch.git` 或者下 `zip包`
-2.  引入dist目录下文件到项目里面，当使用 $.ajax 所有请求自动取消重复请求
-3.  基于 $.ajax 的属性扩展，取消这个功能，只需要在 $.ajax 参数属性里面加 `isAutoCancel: false`
 ```
-<!-- 引入插件！注意要在引入jquery之后再引入它 -->
-<script src="../dist/jquery-fetch.min.js"></script>
-<script>
-  var requestTimer
-  var requestNum = 10
-  requestTimer = setInterval(function () {
-    requestNum --
-    if (requestNum === 0) {
-      clearInterval(requestTimer)
-    }
-    $.ajax('https://easy-mock.com/mock/5bf75efb5cfdd6564a60da75/example/mock',{
-      isAutoCancel: true, // true: 自动取消重复请求(默认), false: 正常ajax请求
-      success: function (data) {
-        console.log('请求成功：', data)
-      }
-    })
-  })
-</script>
+	<script src="../dist/validators.min.js"></script>
 ```
+
+- 法人和其他组织统一社会信用代码
+```
+  if (validators.isUSCI('91350100M000100Y43')) {
+	console.log('✅正确')
+  } else {
+    console.log('✅错误')
+  }
+```
+
+- 身份证号码
+
+```
+  if (validators.isID('91350100M000100Y43')) {
+	console.log('✅正确')
+  } else {
+    console.log('✅错误')
+  }
+```
+
 
 # License
 [MIT](http://opensource.org/licenses/MIT)
--- src/validators.js
+++ src/validators.js
@@ -1,132 +1,215 @@
-/**
- * @description 请求工具类
- * @dependencies ['jQuery']
- */
+void function (global, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory()
+  } else if (typeof define === 'function' && define.amd) {
+    define(factory)
+  } else {
+    global.validators = factory()
+  }
+}(this, function () {
+  var validators = function () {
+    return new validators.fn.init()
+  }
+  var _validatorsPrototype = validators.fn = validators.prototype = {
+    constructor: validators,
+    init: function () { return this }
+  };
+  validators.fn.init.prototype = validators.fn
+  validators.extend = validators.fn.extend = _extend
 
-!function (global, $) {
-  'use strict'
-
-  initTools()
-
-  var pending = []
-  var $ajax = $.ajax
-
-  function fetch (url, set) {
-    var settings = {}
-    var baseURL = global.FETCH_SERVER_URL || ''
-    var fullURL = ''
-    if (typeof url === 'object') {
-      settings = Object.assign({}, settings, url)
-      if (url.url) {
-        fullURL = url.url.indexOf(baseURL) == -1 ? baseURL + url.url : url.url
+  function _extend () {
+    var args = [].slice.call(arguments)
+    var type = args[args.length - 1]
+    var merge = function (obj1, obj2) {
+      if (!_isObject(obj1) || !_isObject(obj2)) {
+        console.warn('合并对象数据类型是Object')
+        return obj1
       }
-    }
-    if (typeof set === 'object') {
-      settings = Object.assign({}, settings, set)
-      if (set.url) {
-        fullURL = set.url.indexOf(baseURL) == -1 ? baseURL + set.url : set.url
+      for (var key in obj2) {
+        obj1[key] = obj2[key]
       }
+      return obj1
     }
-    if (typeof url === 'string') {
-      fullURL = url.indexOf(baseURL) == -1 ? baseURL + url : url
+    if (args.length == 0) {
+      return this
     }
-    settings.url = fullURL
-    // 整理回调函数相关
-    var error  = settings.error
-    var success = settings.success
-    var complete = settings.complete
-
-    // 获取配置项参数
-    var method = settings.type || 'get'
-    var isAutoCancel = settings.isAutoCancel
-
-    // 设置参数格式
-    if (settings.type == 'post') {
-      settings.contentType = settings.contentType || 'application/json; charset=UTF-8'
-      if (settings.data != null && settings.contentType.indexOf('application/json') != -1 && typeof settings.data != 'string') {
-        settings.data = JSON.stringify(settings.data)
+    if (type == 'all' || args.length == 1) {
+      if (!_isObject(args[0])) {
+        console.warn('扩展参数数据类型是Object')
+        return this
       }
-    }
-
-    // 生成取消令牌
-    var brand = window.encodeURIComponent(fullURL + '&' + method)
-
-    // 取消执行
-    if (isAutoCancel == true) {
-      cancelFetch(brand)
-    }
-    // 执行请求相关
-    var ajaxInstance = $ajax(Object.assign({}, settings, {
-      success: function (data) {
-        success && success(data)
-      },
-      error: function (data) {
-        error && error(data)
-      },
-      complete: function (data) {
-        if (isAutoCancel == true) {
-          for (var i = 0; i < pending.length; i++) {
-            if (pending[i]._brand == brand) {
-              pending.splice(i, 1)
-            }
+      for (var key in args[0]) {
+        this[key] = args[0][key]
+        if (type == 'all') {
+          if (this.constructor === validators) {
+            validators[key] = args[0][key]
+          } else {
+            _validatorsPrototype[key] = args[0][key]
           }
         }
-        complete && complete(data)
       }
-    }))
-    // 将执行之后的实例添加到请求记录数组
-    if (isAutoCancel == true) {
-      ajaxInstance._brand = brand
-      pending.push(ajaxInstance)
+      return this
+    } else {
+      for (var i = 1; i < args.length; i++) {
+        merge(args[0], args[i])
+      }
+      return args[0]
     }
-    return ajaxInstance
   }
 
   /**
-   * @param brand {*} 不传参数取消所有可以取消请求
+   * @description 校验Object类型
+   * @param value {any} 验证参数
+   * @return {boolean}
    */
-  function cancelFetch (brand) {
-    for (var i = 0; i < pending.length; i++) {
-      if (brand == null) { // 如果为空，删除所有
-        pending[i].abort() // 执行取消操作
-        pending.splice(i, 1) // 把这条记录从数组中移除
-      } else if (pending[i]._brand === brand) {
-        pending[i].abort()
-        pending.splice(i, 1)
-      }
-    }
+  function _isObject (value) {
+    return Object.prototype.toString.call(value) === '[object Object]'
   }
 
-  // 初始化需要的工具类
-  function initTools () {
-    if (typeof Object.assign != 'function') {
-      Object.assign = assign
+  /**
+   * @description 校验Array类型
+   * @param value {any} 验证参数
+   * @return {boolean}
+   */
+  function _isArray (value) {
+    return Object.prototype.toString.call(value) === '[object Array]'
+  }
+
+  /**
+   * @description 校验身份证 （计算规则参考“中国国家标准化管理委员会”官方文档：http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E）
+   * @param value {string} 验证参数
+   * @return {boolean}
+   */
+  function _isID (value) {
+    if (typeof value != 'string') {
+      console.warn('参数类型错误：参数类型为String')
+      return false
     }
-    function assign (target) {
-      if (Object.prototype.toString.call(target) != '[object Object]') { // TypeError if undefined or null
-        throw new TypeError('Cannot convert not object to object');
-      }
-      var result = target
-      for (var index = 1; index < arguments.length; index++) {
-        var nextObject = arguments[index]
-        if (Object.prototype.toString.call(nextObject) == '[object Object]') { // Skip over if undefined or null
-          for (var nextKey in nextObject) {
-            // Avoid bugs when hasOwnProperty is shadowed
-            if (Object.prototype.hasOwnProperty.call(nextObject, nextKey)) {
-              result[nextKey] = nextObject[nextKey]
-            }
-          }
+    // a1与对应的校验码对照表，其中key表示a1，value表示校验码，value中的10表示校验码X
+    var a1Map = {
+      0: 1,
+      1: 0,
+      2: 10,
+      3: 9,
+      4: 8,
+      5: 7,
+      6: 6,
+      7: 5,
+      8: 4,
+      9: 3,
+      10: 2
+    };
+    var idStr = value.toUpperCase()
+    var reg = /^[0-9]{17}[0-9X]$/
+    if (!reg.exec(idStr)) {
+      return false
+    }
+    var sum = 0
+    var signChar = ''
+    for (var index = 0; index < idStr.length; index++) {
+      var i = 18 - index
+      var c = idStr.charAt(index)
+      var v, weight
+      if (i != 1) {
+        v = parseInt(c)
+        if (!isNaN(v)) {
+          // 计算加权因子
+          weight = parseInt(Math.pow(2, parseFloat(i - 1))) % 11
+          sum += (v * weight)
         } else {
-          console.warn(nextObject, 'Cannot convert not object to object')
+          return false
         }
+      } else {
+        signChar = c.toString()
       }
-      return result
     }
+    var a1 = a1Map[sum % 11]
+    var a1Str = a1.toString()
+    if (a1 == 10) {
+      a1Str = 'X'
+    }
+    return a1Str == signChar
   }
 
-  // 绑定到jQuery静态方法上
-  $.extend({
-    ajax: fetch
-  })
+  /**
+   * @description 校验法人和其他组织统一社会信用代码的合法性 （// 计算规则参考“中国国家标准化管理委员会”官方文档：http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=24691C25985C1073D3A7C85629378AC0）
+   * @param value {string} 验证参数
+   * @return {boolean}
+   */
+  function _isUSCI (value) {
+    if (typeof value != 'string') {
+      console.warn('参数类型错误：参数类型为String')
+      return false
+    }
+    // 代码字符对应的值
+    var valueMap = {
+      '0': 0,
+      '1': 1,
+      '2': 2,
+      '3': 3,
+      '4': 4,
+      '5': 5,
+      '6': 6,
+      '7': 7,
+      '8': 8,
+      '9': 9,
+      'A': 10,
+      'B': 11,
+      'C': 12,
+      'D': 13,
+      'E': 14,
+      'F': 15,
+      'G': 16,
+      'H': 17,
+      'J': 18,
+      'K': 19,
+      'L': 20,
+      'M': 21,
+      'N': 22,
+      'P': 23,
+      'Q': 24,
+      'R': 25,
+      'T': 26,
+      'U': 27,
+      'W': 28,
+      'X': 29,
+      'Y': 30
+    }
+    var usciStr = value.toUpperCase()
+    var reg = /^[A-Z0-9]{18}$/
+    if (!reg.exec(usciStr)) {
+      return false
+    }
+    var weight = 0, sum = 0, index = 0, c, v, usciStrRange = usciStr.slice(0, 17)
+    for (index = 0; index < usciStrRange.length; index++) {
+      c = usciStrRange.charAt(index)
+      v = valueMap[c]
+      weight = parseInt(Math.pow(3, parseFloat(index))) % 31 // 计算加权因子
+      sum += v * weight
+    }
+    var mod = sum % 31
+    var sign = 31 - mod
+    if (sign == 31) {
+      sign = 0
+    }
+    var signChar
+    for (var key in valueMap) {
+      signChar = key
+      if (valueMap[key] == sign) {
+        break
+      }
+    }
+    var lastStr = usciStr.slice(17, 18)
+    var signStr = signChar.toString()
+    return signStr == lastStr
+  }
 
-}(window, window.$)
+  validators.extend({
+    isID: _isID,
+    isUSCI: _isUSCI,
+    isArray: _isArray,
+    isObject: _isObject
+  }, 'all')
+
+  return validators
+})
