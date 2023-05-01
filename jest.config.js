80a335fbb0144335da26012f7a5c9499a4ed8f9b
ztz2 <cn80393313@gmail.com>
2023-05-01 19:03:22 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,14 @@ module.exports = {
   transform: {
     "^.+\.(jsx|tsx|ts|cjs|js|html)$": "ts-jest",
   },
+  // 测试覆盖范围
   collectCoverageFrom: [
     "**/*.test.{js,jsx,tsx,ts}",
     "**/*__test__/*.{js,jsx,tsx,ts}"
+  ],
+  // 忽略项
+  testPathIgnorePatterns: [
+    '/test/.test.ts'
   ],
   cacheDirectory: 'node_modules/.cache/jest',
   // 下面非要从重要, 将不忽略 lodash-es, other-es-lib 这些es库, 从而使ts-jest去处理它们
